ee92e46d01baa7b2161f175b852122783476d5d6
Stratmann <JStratma@sport1.de>
2017-03-30 18:03:58 +0200

    Navigation in Foundation eingebaut

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,16 +1,36 @@
 var React = require('react');
-var {Link} = require('react-router');
+var {IndexLink, Link} = require('react-router');
 
-var Nav = () => {
-  return (
-    <div>
-      <h2>Nav Component</h2>
-      <Link to="/">GetWeather</Link>
-      <Link to="/about">About</Link>
-      <Link to="/examples">Examples</Link>
-    </div>
-  );
-}
+var Nav = React.createClass ({
+  onSearch: function (e) {
+    e.preventDefault();
+    alert('Not yet implemented');
+  },
+  render: function () {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className='menu-text'>React Weather App</li>
+            <li><IndexLink to="/" activeClassNmae="active" activeStyle={{fontWeight: 'bold'}}>GetWeather</IndexLink></li>
+            <li><Link to="/examples" activeClassNmae="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link></li>
+            <li><Link to="/about" activeClassNmae="active" activeStyle={{fontWeight: 'bold'}}>About</Link></li>
+          </ul>
 
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li><input type="search" placeholder="Search"></input></li>
+              <li><input type="submit" className="button" value="Get Weather"></input></li>
+            </ul>
+          </form>
+        </div>
+
+      </div>
+    );
+
+  }
+})
 
 module.exports = Nav;
