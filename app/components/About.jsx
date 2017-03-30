aca6d564ed379d94372b32a9635291ad3a64b992
Stratmann <JStratma@sport1.de>
2017-03-30 16:55:29 +0200

    About + Text

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,5 +1,10 @@
 var React = require('react');
 
-var About = (props) => <h2>About Component</h2>;
+var About = (props) => {
+  return ( <div>
+    <h2>About Component</h2>
+    <p> Welcome to the About page! </p>
+  </div> );
+};
 
 module.exports = About;
