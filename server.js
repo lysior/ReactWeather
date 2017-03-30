2c0c061d9571d2679dff370151e72df7b0d11190
Stratmann <JStratma@sport1.de>
2017-03-30 16:27:19 +0200

    prepare for heroku

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req,res){
+  if (req.headers['X-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname +req.url);
+  }
+
+});
 
 app.use(express.static('public'));
 
@@ -8,6 +18,6 @@ app.get('/', function (req,res) {
 
 });
 
-app.listen(3000, function() {
-  console.log('Server up on 3000');
+app.listen(PORT, function() {
+  console.log('Server up on '+ PORT);
 });
